3590b2ad6698c73e71e0e64d62bb6b910ee7e4c0
Blaise Tyree <5tyreb73@solent.ac.uk>
2023-11-02 10:06:31 +0000

    added extra pages for web application

--- src/main/java/org/solent/devops/user/spring/map/MapApplication.java
+++ src/main/java/org/solent/devops/user/spring/map/MapApplication.java
@@ -1,26 +1,19 @@
-package org.solent.devops.user.spring.map;
+package org.solent.devops.user.spring.web;
 
-import org.solent.devops.user.model.MapPoint;
-import org.solent.devops.user.spring.map.repository.MapPointRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class MapApplication extends SpringBootServletInitializer {
+@SpringBootApplication(scanBasePackages = "org.solent.com619.devops.user.spring.web")
+public class SpringBootJspApplication extends SpringBootServletInitializer {
 
-	@Bean
-	CommandLineRunner runner(MapPointRepository mapPointRepository) {
-		return args -> {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootJspApplication.class);
+    }
 
-			MapPoint point1 = new MapPoint("Solent University", "We go here", "University", 50.90857185116952, -1.4004158721838138);
-			mapPointRepository.save(point1);
-		};
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(MapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJspApplication.class);
+    }
 }
